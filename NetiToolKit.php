8d03a5a07a914b501b671a0be2fe00d0796d5cd2
Hendrik Rombach <hr@netinventors.de>
2016-08-30 18:26:34 +0200

    refactor for 5.2 plugin system (almost done)

---
+++ NetiToolKit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hrombach
+ * Date: 8/30/16
+ * Time: 5:40 PM
+ */
+
+namespace NetiToolKit;
+
+use Shopware\Components\Plugin;
+
+class NetiToolKit extends Plugin
+{
+}
