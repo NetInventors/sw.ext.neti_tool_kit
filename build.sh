c01eb4a3ca967a827e915a1cf3624e54cf414d17
Tobias Pierschel <tp@netinventors.de>
2016-09-07 18:20:11 +0200

    Updated BuildScript

--- build.sh
+++ build.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 WORKING_DIR=$(dirname $0)
-VERSION=$(php -r "echo json_decode(file_get_contents('${WORKING_DIR}/plugin.json'), true)['currentVersion'];")
-TEMP_DIR='/tmp/NetiToolKit/Core/NetiToolKit'
+PLUGIN_NAME=$(basename ${WORKING_DIR})
+VERSION=$(php -r "echo preg_replace('/.*<version>([^<]+)<\/version>.*/ims', '\\1', file_get_contents('${WORKING_DIR}/plugin.xml'), 1);")
+TEMP_DIR='/tmp/ShopwarePlugins/'${PLUGIN_NAME}
 CURRENT_DIR=$(pwd)
-if [ -f "${CURRENT_DIR}/NetiPurchaseHistory-${VERSION}.zip" ]; then
-    rm "${CURRENT_DIR}/NetiPurchaseHistory-${VERSION}.zip"
+EXCLUDES="sftp-config.json nbproject .idea"
+
+# Remove existing package file
+if [ -f "${CURRENT_DIR}/${PLUGIN_NAME}-${VERSION}.zip" ]; then
+    rm "${CURRENT_DIR}/${PLUGIN_NAME}-${VERSION}.zip"
 fi
-mkdir -p $TEMP_DIR
-cp -Rp $WORKING_DIR/* $TEMP_DIR
+
+# Create temporary build folder
+mkdir -p ${TEMP_DIR}
+
+# Copy all files from source folder to temporary folder
+cp -Rp ${WORKING_DIR}/* ${TEMP_DIR}
+
+# Remove build.sh from temporary folder
 rm "${TEMP_DIR}/build.sh"
-if [ -f "${TEMP_DIR}/sftp-config.json" ]; then
-    rm "${TEMP_DIR}/sftp-config.json"
-fi
-if [ -d "${TEMP_DIR}/nbproject/" ]; then
-    rm "${TEMP_DIR}/nbproject/" -R
-fi
-cd $(dirname $(dirname $TEMP_DIR))
-zip -qr "${CURRENT_DIR}/NetiToolKit-${VERSION}.zip" $(basename $(dirname $TEMP_DIR))
-cd $CURRENT_DIR
-echo "Package wurde erstellt unter ${CURRENT_DIR}/NetiToolKit-${VERSION}.zip"
-rm -R $(dirname $(dirname $TEMP_DIR))
+
+# Remove excludes from temporary folder
+for i in ${EXCLUDES}; do
+    if [ -e "${TEMP_DIR}/$i" ]; then
+        rm -R "${TEMP_DIR}/$i"
+    fi
+done
+
+# Step into the temporary build folder
+cd ${TEMP_DIR}
+
+# Create md5.json file including the md5 checksum of every file in the package
+echo "<?php return [" > md5checksum.php
+for i in $(find -path ./.git -prune -o -name build.sh -prune -o -type f -printf '%P\n'); do
+    echo "'$i' => '$(md5sum $i | awk '{ print $1 }')'," >> md5checksum.php
+done;
+echo "];" >> md5checksum.php
+
+# Move one level up in folder structure
+cd ..
+
+# Zip temporary folder contents to package file in plugin folder
+zip -qr "${CURRENT_DIR}/${PLUGIN_NAME}-${VERSION}.zip" $(basename ${TEMP_DIR})
+
+# Step back into the current plugin folder
+cd ${CURRENT_DIR}
+
+echo "Package wurde erstellt unter ${CURRENT_DIR}/${PLUGIN_NAME}-${VERSION}.zip"
+
+# Remove temporary build folder
+rm -R $(dirname ${TEMP_DIR})
