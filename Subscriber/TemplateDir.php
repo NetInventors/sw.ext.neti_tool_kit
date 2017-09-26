c01f4a833ef8b123f4d7c0d75730d2833d825a6a
Hendrik Rombach <hr@netinventors.de>
2017-09-26 16:44:35 +0200

    Add TemplateDir subscriber

---
+++ Subscriber/TemplateDir.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @copyright  Copyright (c) 2017, Net Inventors GmbH
+ * @category   Shopware
+ * @author     hrombach
+ */
+
+namespace NetiToolKit\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+
+class TemplateDir implements SubscriberInterface
+{
+    /**
+     * @var string
+     */
+    private $pluginDir;
+
+    /**
+     * TemplateDir constructor.
+     *
+     * @param string $pluginDir
+     */
+    public function __construct($pluginDir)
+    {
+        $this->pluginDir = $pluginDir;
+    }
+
+    /**
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatchSecure_Frontend_Detail' => 'onPostDispatchSecureFrontendDetail'
+        ];
+    }
+
+    public function onPostDispatchSecureFrontendDetail(\Enlight_Controller_ActionEventArgs $args)
+    {
+        $args->getSubject()->View()->addTemplateDir($this->pluginDir . '/Resources/views');
+    }
+}
