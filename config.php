a7f642346bb655e280fc79e8c803fa2a988a359b
Hendrik Rombach <hr@netinventors.de>
2017-09-26 16:43:35 +0200

    Remove unused attributes

--- config.php
+++ config.php
@@ -14,40 +14,14 @@ return [
     ],
     'attributes' => [
         [
-            'table'  => 's_filter_attributes',
-            'prefix' => 'neti_tool_kit',
-            'suffix' => 'display_in_frontend',
-            'type'   => 'boolean',
-            'data'   => [
-                'label'            => [
-                    'de_DE' => 'Im Frontend ausgeben',
-                    'en_GB' => 'Display in frontend',
-                ],
-                'displayInBackend' => true
-            ]
-        ],
-        [
             'table'  => 's_filter_options_attributes',
             'prefix' => 'neti_tool_kit',
-            'suffix' => 'display_in_frontend',
+            'suffix' => 'hide_in_frontend',
             'type'   => 'boolean',
             'data'   => [
                 'label'            => [
-                    'de_DE' => 'Im Frontend ausgeben',
-                    'en_GB' => 'Display in frontend',
-                ],
-                'displayInBackend' => true
-            ]
-        ],
-        [
-            'table'  => 's_filter_values_attributes',
-            'prefix' => 'neti_tool_kit',
-            'suffix' => 'display_in_frontend',
-            'type'   => 'boolean',
-            'data'   => [
-                'label'            => [
-                    'de_DE' => 'Im Frontend ausgeben',
-                    'en_GB' => 'Display in frontend',
+                    'de_DE' => 'Im Frontend verstecken',
+                    'en_GB' => 'Hide in frontend',
                 ],
                 'displayInBackend' => true
             ]
